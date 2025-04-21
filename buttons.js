be479faf2fc99e661b20730bbdfdad7681059c9c
Emma Halsey <160976732+emhalsey@users.noreply.github.com>
2025-04-21 14:02:21 -0400

    made script file

---
+++ buttons.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    var input1 = prompt("Enter a color: ");
+    var color1 = input1
+    
+    var input2 = prompt("Enter a color: ");
+    var color2 = input2
+    
+    var input3 = prompt("Enter a color: ");
+    var color1 = input3
+
+    document.getElementById('color1').innerHTML = color1;
+    document.getElementById('color2').innerHTML = color1;
+    document.getElementById('color3').innerHTML = color1;
+};
