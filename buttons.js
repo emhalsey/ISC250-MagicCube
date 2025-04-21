0196070e4e0b35e8b455d39dae427b9fb714a535
Emma Halsey <160976732+emhalsey@users.noreply.github.com>
2025-04-21 14:24:04 -0400

    connecting script

--- buttons.js
+++ buttons.js
@@ -1,14 +1,21 @@
-window.onload = function(){
-    var input1 = prompt("Enter a color: ");
-    var color1 = input1
-    
-    var input2 = prompt("Enter a color: ");
-    var color2 = input2
-    
-    var input3 = prompt("Enter a color: ");
-    var color1 = input3
+// provides a prompt box on load, not what we want but might be helpful
 
-    document.getElementById('color1').innerHTML = color1;
-    document.getElementById('color2').innerHTML = color1;
-    document.getElementById('color3').innerHTML = color1;
-};
+// window.onload = function(){
+
+//     var input1 = prompt("Enter a color: ");
+//     var color1 = input1
+    
+//     var input2 = prompt("Enter a color: ");
+//     var color2 = input2
+    
+//     var input3 = prompt("Enter a color: ");
+//     var color1 = input3
+
+//     document.getElementById('color1').innerHTML = color1;
+//     document.getElementById('color2').innerHTML = color1;
+//     document.getElementById('color3').innerHTML = color1;
+// };
+
+randomButton = function(){
+    
+}
