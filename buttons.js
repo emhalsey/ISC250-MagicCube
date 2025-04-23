c8e1984bd9b5e02804bc64792385fb8a1d2e0d0a
domoba <domoba@oswego.edu>
2025-04-23 15:03:22 -0400

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -20,17 +20,31 @@
     //document.getElementsByClassName("color1").className = "red";
 //}
 
-
 function randomButton() {
-    const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'white', 'black'];
-    const color1 = document.querySelectorAll('#amongus td');
+    const colorOptions = ['red', 'orange', 'yellow', 'green', 'blue', 'white', 'black'];
 
-    color1.forEach(cell => {
-        // Remove existing color classes
-        colors.forEach(color => cell.classList.remove(color));
-        
-        // Assign a new random color class
-        const randomColor = colors[Math.floor(Math.random() * colors.length)];
-        cell.classList.add(randomColor);
+    // Shuffle helper
+    function getRandomColor(excludeColors = []) {
+        const availableColors = colorOptions.filter(color => !excludeColors.includes(color));
+        return availableColors[Math.floor(Math.random() * availableColors.length)];
+    }
+
+    const groups = ['color1', 'color2', 'color3'];
+    const usedColors = [];
+
+    groups.forEach(group => {
+        const groupCells = document.querySelectorAll(`.${group}`);
+
+        // Remove any old color classes (from colorOptions list)
+        groupCells.forEach(cell => {
+            colorOptions.forEach(color => cell.classList.remove(color));
+        });
+
+        // Assign a new random color that hasn't already been used
+        const newColor = getRandomColor(usedColors);
+        usedColors.push(newColor);
+
+        // Add that color to all cells in the group
+        groupCells.forEach(cell => cell.classList.add(newColor));
     });
 }
